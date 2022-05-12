10eabb226db701227e94184c6d67384f38ace26e
liuxinxin <cliuxinxin@gmail.com>
2022-05-12 16:05:00 +0800

    坑太多

--- scripts/refine_utils.py
+++ scripts/refine_utils.py
@@ -1,17 +1,54 @@
 import requests
+from data_utils import b_read_dataset
+from scripts.data_utils import ASSETS_PATH
+
+ASSETS_PATH  = '../assets/'
 
 # refine url
-url = '127.0.0.1:3333'
+
+
+file_name = 'compare_results.json'
+file = ASSETS_PATH + file_name
+file
+
+
+def get_csrf_token():
+    url = 'http://127.0.0.1:3333/command/core/get-csrf-token'
+    r = requests.get(url)
+    return r.json()['token']
 
 # 创建project
-def create_project(project_name):
+def create_project(project_name,file_name):
+    url = '127.0.0.1:3333'
     url = 'http://' + url + '/command/core/create-project-from-upload'
+    file = ASSETS_PATH + file_name
     data = {
         'project_name': project_name,
-        'project_file': '',
-        'format':'text/json'
+        'project_file': file_name,
+        'format':'ext/line-based/*sv',
+        'options': {
+            'project_file' : {
+                'file': open(file, 'rb'),
+                'filename': file_name
+            }
+         }
         }
-    r = requests.post(url, data=data)
+    params = {
+        'token': get_csrf_token()
+    }
+    r = requests.get(url, data=data, params=params)
     return r.json()
+
+create_project('test','test.csv')
+
+test = b_read_dataset('compare_results.json')
+import json
+# 保存为json文件
+with open('test.json', 'w', encoding='utf-8') as f:
+    json.dump(test, f, ensure_ascii=False)
+
+import pandas as pd
+test = pd.DataFrame(test)
+test.to_csv(ASSETS_PATH + 'test.csv',index=False)
 
 
--- scripts/test.py
+++ scripts/test.py
@@ -4,29 +4,13 @@ from datetime import datetime
 
 task = 'bid'
 
-b_doccano_train_dev_update(task)
+b_gpu_label(task,'train_dev.json')
 
-train = b_read_dataset('train.json')
-dev = b_read_dataset('dev.json')
+# 去掉 data_source 为na的数据
+db = db[~db['data_source'].isnull()]
 
-col = project_configs[task]['col']
-data_source = project_configs[task]['source']
+b_save_db_basic(db)
 
-cmp = pd.DataFrame(dev)
-# 找到所有的na数据
-na_data = cmp[cmp[col].isna()]
 
-# 如果data_source 为 Nan，就用 source_website_address 填充
-cmp['data_source'] = cmp['data_source'].fillna(cmp['source_website_address'])
-cmp['task'] = cmp['task'].fillna('bid')
-cmp['time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-cmp.drop(['source_website_address'],axis=1,inplace=True)
 
-# 将text改名为data
-cmp.rename(columns={'text':'data'},inplace=True)
 
-b_save_df_datasets(cmp,'train.json')
-b_save_df_datasets(cmp,'dev.json')
-
-b_doccano_upload_by_task('train.json',task,'train')
-b_doccano_upload_by_task('dev.json',task,'dev')
