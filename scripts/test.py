c3d8142a4fb05f0d4e0e6d4285b887f1791ff249
liuxinxin <cliuxinxin@gmail.com>
2022-05-25 10:59:25 +0800

    更新读取程序

--- scripts/test.py
+++ scripts/test.py
@@ -2,42 +2,18 @@ from data_utils import *
 from google_utils import *
 from mysql_utils import *
 
-data = b_read_dataset('train_dev_label.json')
-
-data = pd.DataFrame(data)
-
-data['task'] = 'tender'
-# 去掉file_name
-data = data.drop(['file_name'],axis=1)
-# 去掉 空白 
-data = data.drop([''],axis=1)
-
-b_save_df_datasets(data,'train_dev_label.json')
-
-task = 'bid'
-sql = 'select * from bid_test where project_name is null order by rand() limit 1'
-
-df = mysql_select_df(sql)
-
-labels = json.loads(df.labels[0])
-
-source = project_configs[task]['col']
+project_configs['tender']
 
 
 
-from gne import GeneralNewsExtractor
 
-html = df[source][0]
+b_get_dataprocess()
 
-extractor = GeneralNewsExtractor()
-result = extractor.extract(html)
-print(result)
+document.save(r"D:\test.docx")
 
-nlp = b_load_best_model(task)
-doc = nlp(result['content'])
-for ent in doc.ents:
-    print(ent.text, ent.label_)
+comapare = b_read_db_compare()
 
-sour = project_configs[task]['source']
-df[sour][0]
+# comapare 是df
+# 统计 task 的数量
+comapare.groupby('task').count()
 
