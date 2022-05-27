d867a6c1d79fba3682f075a1991d37b43d803299
liuxinxin <cliuxinxin@gmail.com>
2022-05-27 13:36:27 +0800

    dff data

--- scripts/serve_with_redis.py
+++ scripts/serve_with_redis.py
@@ -7,6 +7,7 @@ import redis
 import glob
 from data_clean import clean_manager
 from process_mysql_data import *
+from get_mysql_data import *
 
 
 # redis 参数设置
@@ -19,9 +20,22 @@ def get_parser():
     return parser
 
 def get_data():
+    """
+    读取所有的数据
+    """
+    process = b_get_dataprocess()
+    for entry in process:
+        task = entry['task']
+        origin_table = entry['origin_table']
+        target_table = entry['target_table']
+        print('task:',task)
+        print('origin_table:',origin_table)
+        print('target_table:',target_table)
+        get_diff_data(task,origin_table,target_table,number=100)
     files = glob.glob(DATA_PATH + '/*.json')
     for file in tqdm(files):
         redis_.rpush('task_export', file)
+    print('get data done')
 
 def parse_data(file):
     print(file)
@@ -29,6 +43,10 @@ def parse_data(file):
     label_data = b_read_dataset(task + '_train_dev.json')
     label_data = pd.DataFrame(label_data)
     df = pd.read_json(file)
+    if df.empty:
+        file_name = file.split('/')[-1]
+        os.rename(file,DATA_PATH + 'processed/' + file_name)
+        return 
     df,std_labels,html_col = preprocess_df(df,task)
     df[html_col] = df[html_col].fillna('')
     df[html_col] = df[html_col].apply(p_filter_tags)
@@ -54,11 +72,15 @@ if __name__ == '__main__':
     number = int(args.number)
     while 1:
         redis_lenght = redis_.llen('task_export')
+        print(f'There is {redis_lenght} files in redis')
+        print('Start to parse data')
         if redis_lenght > 0:
             # 提取数据，数据即文件名
             file = redis_.lpop('task_export').decode('UTF-8')
             parse_data(file)
         time.sleep(60)
+        print('Finish to parse data')
+        print('Start to get data')
         get_data()
 
     
