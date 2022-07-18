8b8dc171ec0973fcf1bcf7dcdd8d6798bd84f9bd
liuxinxin <cliuxinxin@gmail.com>
2022-07-18 17:46:48 +0800

    do some clean

--- scripts/test_general.py
+++
@@ -1,9 +0,0 @@
-from data_utils import b_read_db_compare
-import pytest
-import os
-
-
-def test_compare_have_tender():
-    print(os.getcwd())
-    compare = b_read_db_compare()
-    assert compare.have_tender() is True
