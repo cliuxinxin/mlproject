4858847793f8e81bc072561022491bd9a49a1ad4
lweiya <1078541345@qq.com>
2022-07-19 11:03:15 +0800

    Update get_one_data.py

--- scripts/get_one_data.py
+++ scripts/get_one_data.py
@@ -1,54 +1,41 @@
 # 数据库抽取批量或一条数据然后进行纠错
 import argparse
+from matplotlib.pyplot import table
 import pandas as pd
 from data_utils import*
 from mysql_utils import mysql_select_df
-table_config= {"test_other_tender_bid_result":{
-        "target": "final_other_tender_bid_result",
-        "task": "bid"
-    },
-    "test_other_tender_bid": {
-        "target": "final_other_tender_bid", 
-        "task": "tender"
-    },
-    "test_procurement_bid": {
-        "target": "final_procurement_bid", 
-        "task": "tender"
-    },
-    "test_procurement_bid_result": {
-        "target": "final_procurement_bid_result", 
-        "task": "bid"
-    },
-    "test_tender_bid": {
-        "target": "final_tender_bid", 
-        "task": "tender"
-    },
-    "test_tender_bid_result": {
-        "target": "final_tender_bid_result", 
-        "task": "bid"
-    }}
+
+
+ 
+def get_parser():
+        """
+    脚本文件参数解析
+    """
+        parser = argparse.ArgumentParser(description="Read data from mysql and save to json")
+        parser.add_argument('--task', default='', help='task name')
+        parser.add_argument('--id', default='', help='id')
+        parser.add_argument('--table', default='', help='save 100 records to a file')
+        return parser
 
 if __name__ == '__main__':  
- 
- 
-  table = input("请输入数据库表名：") 
+  parser = get_parser()
+  args = parser.parse_args()
+  task = args.task
+  id = args.id
+  table = args.table
+#   table = input("请输入数据库表名：") 
+#   tablex=table.replace('final','test')
+#   id = input("请输入id:")
+#   task=table_config[tablex]["task"]
   tablex=table.replace('final','test')
-  id = input("请输入id:")
-  task=table_config[tablex]["task"]
   col = 'detail_content'
   source = 'source_website_address'
   sql = f"select * from {table} where id = '{id}'"
   # sql = f"select * FROM final_other_tender_bid_result WHERE CHAR_LENGTH(labels)>500  LIMIT 1"
   # sql = f"select {col},{source} FROM final_other_tender_bid_result WHERE CHAR_LENGTH(labels)>500  LIMIT 1"
   df = mysql_select_df(sql)
-  # 判断拿出的值是否为空
-  # if len(df) >= 1:
   df.to_json(DATA_PATH + f"{task}#{tablex}#{id}.json")
-  #   print("Get %s data from mysql and save to json" % len(df))
-  # else:
-  #   print("No data")
   data=df[[col,source]]
-
   a=p_process_df(data,task)
   b_save_df_datasets(a,"one_data.json")
   # 加入标签
