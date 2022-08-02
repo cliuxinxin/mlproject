6752acda3a714907528a85a1cc49b0ce8c9e4b38
liuxinxin <cliuxinxin@gmail.com>
2022-08-02 14:51:57 +0800

    test

--- scripts/test.py
+++ scripts/test.py
@@ -12,36 +12,13 @@ tabels = [
     'test_tender_bid'
 ]
 
-def read_file(file):
-    df = pd.read_csv(ASSETS_PATH + file,header=None)
-    df.columns = ['source_website_name','table']
-    df['pk'] = df['source_website_name'] + '_' + df['table']
-    return df
+keyword = '项目建议书'
 
-def write_data(df,file):
-    file = ASSETS_PATH + file
-    df.to_csv(file, mode='a', header=False, index=False)
+sql = f'select * from {tabels[0]} where title like "{keyword}%" limit 1'
 
-df_source = read_file('table_source.csv')
-df_finish = read_file('finish.csv')
-df_source = df_source[~df_source['pk'].isin(df_finish['pk'])]
+df = mysql_select_df(sql)
 
-
-for source,table,pk in tqdm(df_source.values):
-    sql = f"select id from {table} where source_website_name = '{source}' limit 10"
-    df_id = mysql_select_df(sql)
-    if len(df_id) == 1:
-        sql = f"select '{table}' as table_name,id,source_website_name,source_website_address,detail_content from {table} where id = '{df_id.id.values[0]}'"
-        df = mysql_select_df(sql)
-        write_data(df, 'dev_add.csv')
-    else:
-        train = int(len(df_id) * 0.8)
-        sql = f"select '{table}' as table_name,id,source_website_name,source_website_address,detail_content from {table} where id in {tuple(df_id.id.values.tolist())}"
-        df = mysql_select_df(sql)
-        write_data(df[:train], 'train_add.csv')
-        write_data(df[train:], 'dev_add.csv') 
-    finish_df = pd.DataFrame({'source_website_name':[source],'table':[table]})
-    write_data(finish_df,'finish.csv')
+df.columns
     
     
 
