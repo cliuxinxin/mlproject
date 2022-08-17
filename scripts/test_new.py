b85e428d73c2dbcea888221dd6ac881de11b86d1
liuxinxin <cliuxinxin@gmail.com>
2022-08-17 15:42:35 +0800

    use all models

--- scripts/test_new.py
+++ scripts/test_new.py
@@ -1,3 +1,4 @@
+from posixpath import devnull
 from mysql_utils import *
 from data_utils import *
 
@@ -55,4 +56,64 @@ for table in tables:
                 else:
                     df[:int(len_df*0.8)].to_json(ASSETS_PATH + name + '#train.json')
                     df[int(len_df*0.8):].to_json(ASSETS_PATH + name + '#dev.json')
+
+
+files = os.listdir(ASSETS_PATH + 'bidclass/')
+
+train = []
+dev = []
+
+for file in files:
+    if file == '.DS_Store' or file == '.gitignore':
+        continue
+    paras = file.split('#')
+    is_train = paras[-1] == 'train.json'
+    df = pd.read_json(ASSETS_PATH + 'bidclass/' + file)
+    if is_train:
+        train.append(df)
+    else:
+        dev.append(df)
+
+train = pd.concat(train)
+dev = pd.concat(dev)
+
+# 如果table,id,source,text,task,md5 都一样，则合并label column的值
+train = train.groupby(['table','id','source','text','task','md5'])['label'].apply(lambda x:','.join(x)).reset_index()
+dev = dev.groupby(['table','id','source','text','task','md5'])['label'].apply(lambda x:','.join(x)).reset_index()
+
+train['len'] = train['text'].apply(lambda x:len(x))
+dev['len'] = dev['text'].apply(lambda x:len(x))
+
+# 去掉text为0的
+train = train[train['len'] != 0]
+dev = dev[dev['len'] != 0]
+# 去掉text>50000的
+train = train[train['len'] < 50000]
+dev = dev[dev['len'] < 50000]
+
+b_save_df_datasets(train,'train_bid.json')
+b_save_df_datasets(dev,'dev_bid.json')
+
+
+train = b_read_dataset('train_bid.json')
+devnull = b_read_dataset('dev_bid.json')
+
+for entry in train:
+    labels = entry['label'].split(',')
+    labels = set(labels)
+    labels = list(labels)
+    entry['label'] = labels
+
+for entry in dev:
+    labels = entry['label'].split(',')
+    labels = set(labels)
+    labels = list(labels)
+    entry['label'] = labels
+
+b_save_list_datasets(train,'train_bid_new.json')
+b_save_list_datasets(dev,'dev_bid_new.json')
+
+b_doccano_export_cat_project(37,'train.json')
+b_doccano_export_cat_project(38,'dev.json')
+
 
