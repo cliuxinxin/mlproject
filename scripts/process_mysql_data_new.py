c46fcf1689188d3c495dbf06b952c924a58aa3c0
Lance <75961930+Lance-Owen@users.noreply.github.com>
2022-09-18 17:50:50 +0800

    Update process_mysql_data_new.py

--- scripts/process_mysql_data_new.py
+++ scripts/process_mysql_data_new.py
@@ -234,7 +234,10 @@ class Helper():
 
     def get_label(self,task):
         if task == 'tender':
-            return self.tender_label
+            tender_label = self.tender_label[['md5','label']]
+            tender_label.columns = ['md5','labels']
+            tender_label.set_index('md5',inplace=True)
+            return tender_label
         elif task == 'bid':
             bid_label = self.bid_label[['md5','label']]
             bid_label.columns = ['md5','labels']
@@ -256,6 +259,19 @@ def deal_detail_content(html):
         return repr(content)
     else:
         return ''
+
+def is_full_data(task,df):
+    import numpy as np
+    pd.DataFrame([False,False,False])
+    if list(df[['province','city','county','publish_time','title']] == df[['province','city','county','publish_time','title']]) == [False,False,False,False,False]:
+        return 0
+    elif task =='tender' and not all(x in df['labels'].split("\"") for x in ["项目名称","项目招标编号","预算","招标单位","招标文件领取开始时间","投标截止时间"]):
+        return 0
+    elif task == 'bid' and not all(x in df['labels'].split("\"") for x in ["项目名称","中标公告编号","中标金额","中标单位"]):
+        return 0
+    else:
+        return 1
+    
 if __name__ == '__main__':
     helper = Helper()
     data_process = json.loads(open('data_process.json','r',encoding='utf-8').read())
@@ -335,7 +351,9 @@ if __name__ == '__main__':
                 df['labels'] = df['labels'].apply(lambda x: x[:1000])
                 df = df.drop(columns=['md5','text','clean_labels'])
                 df = datetime_process(df,task)
-
+                if origin_table in ['test_tender_bid','test_tender_bid_result']:
+                    df['is_full_data'] = df.apply(lambda x:is_full_data(task,x),axis=1)                  
+                    
                 # 填写数据
                 delete_and_insert_target(file, target_table, df)
             except Exception as e:
