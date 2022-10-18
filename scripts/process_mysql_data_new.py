bfe47ddaa4c8176f040cc50f11cae8de0b597ba8
lweiya <1078541345@qq.com>
2022-10-18 15:48:15 +0800

    Update process_mysql_data_new.py

--- scripts/process_mysql_data_new.py
+++ scripts/process_mysql_data_new.py
@@ -331,7 +331,7 @@ if __name__ == '__main__':
                 df['text'] = df['text'].str[:max_len]
                 # 计算标签
                 data = df['text'].to_list()
-                docs = nlp.pipe(data,n_process=1)
+                docs = nlp.pipe(data)
                 labels = []
 
                 for doc in docs:
