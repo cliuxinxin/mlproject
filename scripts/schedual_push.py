a2816eb81780a790a5fa769668d3f61775fc33ce
Lance <75961930+Lance-Owen@users.noreply.github.com>
2023-01-16 18:21:25 +0800

    Update schedual_push.py

--- scripts/schedual_push.py
+++ scripts/schedual_push.py
@@ -1,4 +1,4 @@
-from redis_utils import redis_,diff_key,redis_push_diff,ori_tar_configs,tag_key,redis_push_tag,tag_configs
+from redis_utils import redis_,diff_key,redis_push_diff,ori_tar_configs,tag_key,redis_push_tag,tag_configs,redis_push_all_tender
 import time
 
 
@@ -8,7 +8,8 @@ def is_redis_not_enough(key,length):
 while True:
     if is_redis_not_enough(diff_key,1000):
         try:
-            redis_push_diff(ori_tar_configs)
+            # redis_push_diff(ori_tar_configs)
+            redis_push_all_tender()
             print(f'There is {redis_.llen(diff_key)} data in redis')
         except:
             continue
@@ -18,4 +19,5 @@ while True:
             print(f'There is {redis_.llen(tag_key)} data in redis')
         except:
             continue
+    break
     time.sleep(60)
