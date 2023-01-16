460666736ae8fe28dafea7d7bcddbd32a85588b5
Lance <75961930+Lance-Owen@users.noreply.github.com>
2023-01-16 18:17:53 +0800

    Update redis_utils.py

--- scripts/redis_utils.py
+++ scripts/redis_utils.py
@@ -80,6 +80,15 @@ def redis_push_tag_test(tag_configs):
         df['task'] = task
         redis_push(df,tag_key)
 
+def redis_push_all_tender():
+    for ori in ['test_tender_bid','test_procurement_bid','test_other_tender_bid']:
+        # 读取所有id数据
+        sql = f'select id from {ori}'
+    df = mysql_select_df(sql)
+    df['table'] = ori
+
+    redis_push(df,diff_key)
+
 def redis_not_empty(key):
     return redis_.llen(key) > 0
 
